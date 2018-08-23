c75e9e1d7698dd000936307cc6ffb7d828a49749
Graham Hency <ghency@Fitzmark.com>
2018-08-23 13:31:44 -0400

    add: search and sort

--- assets/javascripts/components/device-table/device-table.component.js
+++ assets/javascripts/components/device-table/device-table.component.js
@@ -13,9 +13,47 @@
 
     // Scope Variables
     vm.collection = [];
+    vm.rawRows = [];
+    vm.searchTerm;
+    vm.sort = "Model";
+    vm.sortIndex = 0;
+    vm.sortDir = 1;
+    vm.sortDirOpts = [
+      { 'val': 1, 'label': 'asc' },
+      { 'val': -1, 'label': 'desc'}
+    ];
 
     // Scope Functions
+    vm.search = function() {
+      if(vm.searchTerm){
+        vm.collection.rows = vm.rawRows.filter(function(row){
+          return filterRow(row);
+        });
+      } else {
+        if(vm.sort) vm.rawRows.sort(sortRows);
+        vm.collection.rows = vm.rawRows;
+      }
+    }
 
+    vm.sortChange = function(){
+      vm.sortIndex = vm.collection.headers.indexOf(vm.collection.headers.find(i => i.label == vm.sort));
+      vm.collection.rows.sort(sortRows);
+    }
+
+    function sortRows(a, b){
+      if(vm.sortDir === 1){
+        return a[vm.sortIndex] > b[vm.sortIndex] ? 1 : -1;
+      } else {
+        return b[vm.sortIndex] > a[vm.sortIndex] ? 1 : -1;
+      }
+    }
+
+    function filterRow(row){
+      for(var i = 0; i < row.length; i++){
+        if(row[i].toLowerCase().includes(vm.searchTerm.toLowerCase())) return true;
+      }
+      return false;
+    }
 
     // Lifecycle Hooks
     vm.$onInit = onInit;
@@ -25,7 +63,10 @@
     function onInit() {
       deviceFactory.get().then(function(res) {
         vm.collection = res;
+        vm.rawRows = res.rows.slice();
+        vm.sortChange();
       });
     }
+
   }
 })();
